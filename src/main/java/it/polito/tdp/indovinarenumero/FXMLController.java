72fe9a311c424eeddc698a78c580253f68415bbb
Riccardo <Riccardo@DESKTOP-U0PF1S3.home-life.hub>
2020-03-18 12:24:38 +0100

    Primo

---
+++ src/main/java/it/polito/tdp/indovinarenumero/FXMLController.java
@@ -0,0 +1,83 @@
+package it.polito.tdp.indovinarenumero;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private final int NMAX=100;
+	private final int TMAX=8;
+	private int segreto;
+	private int tentativiFatti;
+	boolean inGioco=false;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnNuova;
+
+    @FXML
+    private TextField Rimasti;
+
+    @FXML
+    private TextField txtTentativi;
+
+    @FXML
+    private Button btnProva;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void doNuova(ActionEvent event) {
+    	
+    	//inizio una nuova partita - logica
+    	this.segreto=(int) (Math.random()*NMAX)+1;
+    	this.tentativiFatti=0;
+    	this.inGioco=true;
+    	
+    	//inizio una nuova partita - grafica
+    	
+    	txtRisultato.clear();
+    	txtRisultato.appendText("Partita iniziata\n");
+    	txtRisultato.appendText("Numero estratto "+this.segreto+"\n");
+    	txtTentativi.clear();
+    	Rimasti.setText(Integer.toString(TMAX));
+
+    }
+
+    @FXML
+    void doTentativo(ActionEvent event) {
+    	
+    	//leggo input
+    	String ts=txtTentativi.getText();
+    	int tentativo=0;
+    	tentativo=Integer.parseInt(ts);
+    	this.tentativiFatti++;
+    	
+    	if 
+    	
+    	
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnNuova != null : "fx:id=\"btnNuova\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Rimasti != null : "fx:id=\"Rimasti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTentativi != null : "fx:id=\"txtTentativi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnProva != null : "fx:id=\"btnProva\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+}
+
+
---
+++ src/main/java/it/polito/tdp/indovinarenumero/Main.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.indovinarenumero;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
